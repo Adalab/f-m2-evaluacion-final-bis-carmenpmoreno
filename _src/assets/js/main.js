e2cc5d4197ed4b19da9032c8634cb9a6e31f448f
carmenpmoreno <carmenp.moreno@gmail.com>
2019-05-01 22:50:51 +0200

    add first js logic

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -2,5 +2,16 @@
 
 console.log('>> Ready :)');
 
+// Escuchar el click sobre el container del input y añadirle la clase select__option-checked
+const inputContainers = document.querySelectorAll('.select__option-container');
+
+for (const inputContainerEl of inputContainers) {
+  inputContainerEl.addEventListener('clik', handleInputClick);
+}
+
+function handleInputClick(event) {
+  event.currentTarget.classList.add('selected');
+}
+
 //   <ul class="cards-list"></ul>
 //   <li class="cards-list__item"></li>
