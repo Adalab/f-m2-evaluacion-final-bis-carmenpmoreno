cced9f7eac4fc51fd6977875123be9b997adc3db
carmenpmoreno <carmenp.moreno@gmail.com>
2019-05-02 10:49:49 +0200

    add selected inputs radios logic

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -2,15 +2,24 @@
 
 console.log('>> Ready :)');
 
-// Escuchar el click sobre el container del input y añadirle la clase select__option-checked
+// 1. Escuchar el click sobre el container del input y añadirle la clase select__option-checked
+
+// recojo todos los container del input y creo un array
 const inputContainers = document.querySelectorAll('.select__option-container');
-
+console.log(inputContainers);
+// le pongo a cada uno un listener
 for (const inputContainerEl of inputContainers) {
-  inputContainerEl.addEventListener('clik', handleInputClick);
+  inputContainerEl.addEventListener('click', handleInputClick);
 }
-
+// función handler sobre cada uno
 function handleInputClick(event) {
-  event.currentTarget.classList.add('selected');
+  // console.log(event); se hac un evento por el click del radio y otro por mi adEventListener
+  if (event.currentTarget.classList.contains('selected')) {
+    event.currentTarget.classList.remove('selected');
+  } 
+  else {
+    event.currentTarget.classList.add('selected');
+  }
 }
 
 //   <ul class="cards-list"></ul>
