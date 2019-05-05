3490871fa167adba636f3f30bc692f6f2b5f1f3f
carmenpmoreno <carmenp.moreno@gmail.com>
2019-05-05 21:08:39 +0200

    clean code

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -7,7 +7,6 @@ const savedValue = JSON.parse(localStorage.getItem('value'));
 
 for (let i = 0; i < inputContainers.length; i++) {
   if (inputContainers[i].children[0].value === savedValue) {
-    console.log('input ANTES seleccionado');
     inputContainers[i].classList.add('check');
     inputContainers[i].children[0].classList.add('input-checked');
   }
@@ -16,7 +15,6 @@ for (let i = 0; i < inputContainers.length; i++) {
 for (const inputContainerEl of inputContainers) {
   inputContainerEl.addEventListener('click', handleInputClick);
 }
-
 function handleInputClick(event) {
   if (!event.currentTarget.classList.contains('check')) {
     for (let i = 0; i < inputContainers.length; i++) {
@@ -25,26 +23,17 @@ function handleInputClick(event) {
     }
     event.currentTarget.classList.add('check');
     event.currentTarget.children[0].classList.add('input-checked');
-    // AHORA GUÁRDAMELO TODO EN EL LOCAL STORAGE, PRIMERO RECOJO LAS CONSTANTES QUE NECESITO
     const inputChecked = document.querySelector('.input-checked');
     const value = inputChecked.value;
-    // 3. Guardar el número de cartas pedidas en el Local Storage
     localStorage.setItem('value', JSON.stringify(value));
-    console.log('valor del input guardado: ', value);
-    console.log('valor del input recuperado: ', savedValue);
   }
 }
 
 buttonEl.addEventListener('click', handleButtonClick);
-
 function handleButtonClick() {
   const inputChecked = document.querySelector('.input-checked');
   const value = inputChecked.value;
-  fetch(
-    `https://raw.githubusercontent.com/Adalab/cards-data/master/${
-      inputChecked.value
-    }.json`
-  )
+  fetch(`https://raw.githubusercontent.com/Adalab/cards-data/master/${inputChecked.value}.json`)
     .then(function(response) {
       return response.json();
     })
@@ -56,20 +45,14 @@ function handleButtonClick() {
 function printCards(value, data) {
   mainSection.innerHTML = '<ul class="cards"></ul>';
   const cardsList = document.querySelector('.cards');
-  let backImage =
-    '<img src="https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB" alt="trasera de la carta" class="back-image"/>';
+  let backImage = '<img src="https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB" alt="trasera de la carta" class="back-image"/>';
   for (let i = 0; i < value; i++) {
     cardsList.innerHTML += '<li class="card"></li>';
     const cards = document.querySelectorAll('.card');
-
     for (const card of cards) {
       card.addEventListener('click', handleCardsClick);
     }
-
-    let frontImage = `<img src="${data[i].image}" alt="${
-      data[i].name
-    }" class="front-image hidden"/>`;
-
+    let frontImage = `<img src="${data[i].image}" alt="${data[i].name}" class="front-image hidden"/>`;
     cards[i].innerHTML = backImage + frontImage;
   }
 }
